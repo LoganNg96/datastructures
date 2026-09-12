349f8c520e8ecd7248e81bd846260b0bb39ce7ed
LoganNg96 <logan.maverick.ng@gmail.com>
2026-09-12 16:37:45 -0700

    more practice

--- linkedlists.py
+++ linkedlists.py
@@ -3,19 +3,29 @@ class Node:
         self.data = data
         self.next = None
 
+head = None
+current = None
+file = open("linkedlist_input.txt", "r")
 
+reading_file = True
+while (reading_file):
+    line = file.readline()
+    if (line == ''): # We've hit the end of the file
+        reading_file = False
+        break
 
-amount_of_nodes = int(input("Enter the number of nodes: "))
-nodes = []
-for i in range(amount_of_nodes):
-    data = int(input(f"Enter data for node {i + 1}: "))
-    nodes.append(Node(data))
+    data = int(line)
+    newnode = Node(data)
+    if current == None: # then this is our very first node
+        head = newnode
+    else:
+        current.next = newnode
+    current = newnode
+file.close() #read it in, now must print it out
 
-for i in range(amount_of_nodes - 1):
-    nodes[i].next = nodes[i + 1]
-
-current = nodes[0]
-
+current = head
 while current != None:
     print(current.data)
     current = current.next
+
+
